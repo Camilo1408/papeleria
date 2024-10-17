a5716b0f33f1b3628fc6993553e7ea9567e75ab0
Cesar Camilo <cesarcamilohoyosdiaz@gmail.com>
2024-10-17 13:50:07 -0500

    Se elimino el elemento nosotros

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ export const Nav = () => {
             <ul className="flex justify-around ">
                 <li className="mr-2 ml-2"><a href="#">Inicio</a></li>
                 <li className="mr-2 ml-2"><a href="#">Categorias</a></li>
-                <li className="mr-2 ml-2"><a href="#">Nosotros</a></li>
+        
             </ul>
         </nav>
     </>
