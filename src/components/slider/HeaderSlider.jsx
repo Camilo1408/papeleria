c37234d96ba189fc40dc91a2b8ce8bfda6a61a3a
Cesar Camilo <cesarcamilohoyosdiaz@gmail.com>
2024-10-19 15:31:49 -0500

    Correccion de boton

--- src/components/slider/HeaderSlider.jsx
+++ src/components/slider/HeaderSlider.jsx
@@ -60,7 +60,7 @@ const HeaderSlider = () => {
                                 <p className="text-sm">{data.description}</p>
                                 <div>
                                     <button className=" bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">
-                                        Order Now
+                                        Compra ya
                                     </button>
                                 </div>
                             </div>
