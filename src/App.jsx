e02c321edc862ed44fb8d09b0e2080c0311e7dfe
Alvarito <juliandelgado2201@gmail.com>
2024-10-21 17:41:53 -0500

    seccion productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import Nav from "./components/nav"
+import React from "react";
+import Nav from "./components/Nav"
 import HeaderSlider from "./components/slider/HeaderSlider"
+import Products from "./components/products/Products"
+import AOS from "aos";
+import "aos/dist/aos.css"
 
 function App() {
   
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration:800,
+      easing:"ease-in-sine",
+      delay:100,
+    });
+    AOS.refresh();
+  }, []);
 
   return (
     <>
       <Nav></Nav>
       <HeaderSlider></HeaderSlider>
+      <Products></Products>
     </>
   )
 }
